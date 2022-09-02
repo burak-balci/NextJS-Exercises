b9a4d933e01719878abf2589ab306538da44d8ec
Burak <96511762+burak-balci@users.noreply.github.com>
2022-09-02 19:14:29 +0300

    Trending detail page done

--- movieapp/components/Header/Header.tsx
+++ movieapp/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
-        <div className={styles.title}>BB Movies</div>
+        <div className={styles.title}>
+          <Link href="/">BB Movies</Link>
+        </div>
         <input className={styles.input} type="text" />
       </div>
     </div>
--- movieapp/pages/trendingdetails/[id].tsx
+++ movieapp/pages/trendingdetails/[id].tsx
@@ -1,10 +1,64 @@
 import { useRouter } from "next/router";
 import React from "react";
+import type { InferGetStaticPropsType } from "next";
+import { Trending, Result } from "../../types";
+import styles from "../../styles/TvDetailsPage.module.css";
 
-const TvDetailsPage = () => {
+const TvDetailsPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const route = useRouter();
+  const filtered = data.results.filter(
+    (item: Result) => Number(item.id) === Number(route.query.id)
+  );
+  console.log(filtered[0]);
 
-  return <div>TvDetailsPage {route.query.id}</div>;
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.cover}
+        style={{
+          backgroundImage: `url(https://www.themoviedb.org/t/p/original${filtered[0].backdrop_path})`,
+        }}
+      ></div>
+      <div className={styles.title}>
+        {filtered[0].name || filtered[0].title}
+      </div>
+      <div className={styles.desc}>{filtered[0].overview}</div>
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}`
+  );
+
+  const data = await res.json();
+
+  const paths = data.results.map((item: Result) => {
+    return {
+      params: { id: item.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default TvDetailsPage;
