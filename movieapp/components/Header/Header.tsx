9b0003731e0f70ea3bcdeef8c4c04c6e44ec1036
Burak <96511762+burak-balci@users.noreply.github.com>
2022-09-03 12:25:09 +0300

    searchpage done

--- movieapp/components/Header/Header.tsx
+++ movieapp/components/Header/Header.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const [text, setText] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!text) {
+      return null;
+    }
+    router.push(`/search/${text}`);
+    setText("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
         <div className={styles.title}>
           <Link href="/">BB Movies</Link>
         </div>
-        <input className={styles.input} type="text" />
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            value={text}
+            onChange={handleChange}
+            className={styles.input}
+            type="text"
+            placeholder="Search movies..."
+          />
+        </form>
       </div>
     </div>
   );
---
+++ movieapp/pages/search/[text].tsx
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import React from "react";
+import type { GetServerSideProps } from "next";
+import { PopularMovies, Result } from "../../types";
+import styles from "../../styles/SearchPage.module.css";
+import Card from "../../components/Card";
+
+const SearchPage = ({ results }: { results: Result[] }) => {
+  const route = useRouter();
+
+  return (
+    <div className={styles.innerContainer}>
+      {results.map((movie) => (
+        <Card key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${context.query.text}&page=1&include_adult=false`
+  );
+  const { results }: PopularMovies = await res.json();
+
+  return {
+    props: {
+      results,
+    },
+  };
+};
+
+export default SearchPage;
